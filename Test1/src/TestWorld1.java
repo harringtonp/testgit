bf26d3dbe939236d00fc44ec2e2a2eba96277578
Paul Harrington <pharrington@data-display.com>
2013-06-13 13:05:39 +0100

    T3

--- Test1/src/TestWorld1.java
+++ Test1/src/TestWorld1.java
@@ -5,7 +5,7 @@ public class TestWorld1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.err.println("Hello Test GIT 1");
+		System.err.println("Hello Test GIT 2");
 		System.exit(0);
 	}
 }
