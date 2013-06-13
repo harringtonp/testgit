48b3737f8f4cf2cfddfda7317451f5581ebc86cd
Paul Harrington <pharrington@data-display.com>
2013-06-13 15:48:32 +0100

    e3qe

--- Test1/src/Test1HelloWorld1.java
+++ Test1/src/Test1HelloWorld1.java
@@ -5,7 +5,7 @@ public class Test1HelloWorld1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.err.println("Add New Line");
+		System.err.println("Add New Line2");
 		System.err.println("Test1HelloWorld1");
 		System.exit(0);
 	}
