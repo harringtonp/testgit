c0b8aeda9af00bbcc13a44388d48a2569e9e4cd4
Paul Harrington <pharrington@data-display.com>
2013-06-13 15:29:58 +0100

    T1C3

--- Test1/src/Test1HelloWorld1.java
+++ Test1/src/Test1HelloWorld1.java
@@ -5,7 +5,7 @@ public class Test1HelloWorld1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.err.println("Hello Test GIT 2");
+		System.err.println("Test1HelloWorld1");
 		System.exit(0);
 	}
 }
--- Test1/src/Test1HelloWorld2.java
+++ Test1/src/Test1HelloWorld2.java
@@ -1,11 +1,11 @@
 
-public class TestWorld1 {
+public class Test1HelloWorld2 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.err.println("Hello Test GIT 2");
+		System.err.println("Test1HelloWorld2");
 		System.exit(0);
 	}
 }
