ceaf7fa73cbf312e94fa70fa408005fb45bb77e9
Paul Harrington <pharrington@data-display.com>
2013-06-13 15:38:54 +0100

    t1c1

--- Test1/src/Test1HelloWorld1.java
+++ Test1/src/Test1HelloWorld1.java
@@ -5,6 +5,7 @@ public class Test1HelloWorld1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.err.println("Add New Line");
 		System.err.println("Test1HelloWorld1");
 		System.exit(0);
 	}
